f5051aa6bc8ce5cfb543427ce56d2493640d0c85
Nicolas Boute <4249@holbertonschool.com>
2022-02-04 12:36:12 +0100

    fixed typo in filename

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function prints numbers
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char    c;
+
+	c = '0';
+	while (c <= '9')
+		putchar(c++);
+	c = 'a';
+	while (c <= 'f')
+		putchar(c++);
+	putchar('\n');
+	return (0);
+}
