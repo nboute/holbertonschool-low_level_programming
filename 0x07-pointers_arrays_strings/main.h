da9d903ed4adc9db8a7d6d5833137fb65835eb20
nicolas <4249@holbertonschool.com>
2022-02-21 15:08:30 +0100

    removed extraneous line return in crackme password

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define NULL 0x0
 int				_putchar(char c);
 
 char			*_memset(char *s, char b, unsigned int n);
@@ -11,4 +12,7 @@ char			*_strpbrk(char *s, char *accept);
 char			*_strstr(char *haystack, char *needle);
 void			print_chessboard(char (*a)[8]);
 void			print_diagsums(int *a, int size);
+
+void			set_string(char **s, char *to);
+
 #endif /* MAIN_H */
