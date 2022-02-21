4054a505eab2fa088dd3b27e4ea8e79eb93daca9
nicolas <4249@holbertonschool.com>
2022-02-21 13:38:47 +0100

    fixed error case with strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,10 +10,8 @@ char	*_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i]; i++)
 	{
-		for (j = 0; needle[j] && haystack[i + j]; j++)
+		for (j = 0; haystack[i + j] == needle[j]; j++)
 		{
-			if (needle[j] != haystack[i + j])
-				break;
 			if (needle[j + 1] == '\0')
 				return (haystack + i);
 		}
