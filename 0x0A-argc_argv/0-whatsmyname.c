bad4010d3a5c11a74e7bec032f98358a778e6317
nicolas <4249@holbertonschool.com>
2022-02-25 13:23:22 +0100

    added missing jsdoc for a function

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * main - Prints name of executable
+ * @argc: number of arguments
+ * @argv: array of arguments strings
+ * Return: Always 0
+ */
 int		main(int ac, char **av)
 {
 	(void)ac;
