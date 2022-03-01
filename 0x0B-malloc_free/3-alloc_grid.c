90a844adf054f83d85a2e681fff866b0e054da68
Nicolas Boute <4249@holbertonschool.com>
2022-03-01 13:38:56 +0100

    fixed initalization of array before check for malloc fail

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -64,9 +64,9 @@ int		**alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	tab = malloc(sizeof(int *) * height);
-	_memset((char*)tab, 0, sizeof(int *) * height);
 	if (!tab)
 		return (NULL);
+	_memset((char*)tab, 0, sizeof(int *) * height);
 	for (i = 0; i < height; i++)
 	{
 		tab[i] = malloc(sizeof(int) * width);
