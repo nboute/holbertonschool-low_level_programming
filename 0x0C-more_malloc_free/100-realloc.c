e71ff66ffe60d1ac601e80ec1959ef0b996cc13e
Nicolas Boute <4249@holbertonschool.com>
2022-03-03 12:34:48 +0100

    added first advanced assignment of 0x0C

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+
+/**
+ * _memcpy - copies n bytes from a buffer to another
+ * @dest: destination buffer
+ * @src: source buffer
+ * @n: number of bytes to copy
+ * Return: Destination buffer @dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	long	*ldest, *lsrc;
+	char	*ptr;
+	unsigned int	size;
+
+	ldest = (long *)dest;
+	lsrc = (long *)src;
+	size = sizeof(long);
+	while (n >= size)
+	{
+		*ldest++ = *lsrc++;
+		n -= size;
+	}
+	ptr = (char *)ldest;
+	src = (char *)lsrc;
+	while (n--)
+		*ptr++ = *src++;
+	return (dest);
+}
+
+/**
+ * _realloc - reallocates a buffer with a different size
+ * @ptr: Old buffer
+ * @old_size: Size of old buffer
+ * @new_size: Size of new buffer
+ * Return: New buffer or NULL if error
+ */
+
+void	*_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void	*reptr;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size)
+		reptr = malloc(new_size);
+	if (!reptr)
+		return (NULL);
+	if (ptr && new_size)
+		_memcpy(reptr, ptr, old_size);
+	if (ptr)
+		free(ptr);
+	if (!new_size && ptr)
+		return (NULL);
+	return (reptr);
+}
