c85f2b6d57e1791c3a24d905cde89c586a42b083
nicolas <4249@holbertonschool.com>
2022-03-07 15:55:29 +0100

    fixed typo

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,9 +10,6 @@ void	print_dog(struct dog *d)
 	if (!d)
 		return;
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
-	if (d->age == 0.0f)
-		printf("Age: %f\n", d->age);
-	else
-		printf("Age: (nil)\n");
-	printf("Name: %s\n", d->owner ? d->owner : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
