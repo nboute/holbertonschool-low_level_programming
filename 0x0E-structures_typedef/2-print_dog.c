ce6bd60334b8a106d36e8d439057dd2a47de9c3c
nicolas <4249@holbertonschool.com>
2022-03-07 15:54:24 +0100

    fixed missing error handling

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void	print_dog(struct dog *d)
 	if (!d)
 		return;
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
-	if (d->age)
+	if (d->age == 0.0f)
 		printf("Age: %f\n", d->age);
 	else
 		printf("Age: (nil)\n");
