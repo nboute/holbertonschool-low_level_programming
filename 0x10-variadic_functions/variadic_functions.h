a95446e8338ef5ce7081030423a4748fe9fddeb0
nicolas <4249@holbertonschool.com>
2022-03-10 12:15:49 +0100

    added missing prototype to header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,6 +9,7 @@ typedef struct	arg_s
 	void		(*fct)(va_list *args);
 }				arg_t;
 
+int	_putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
