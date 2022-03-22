ab8444e0db360f094cecf25faf712c7d2435e37a
Nicolas Boute <4249@holbertonschool.com>
2022-03-22 09:54:19 +0100

    fixed out of bounds bug for linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int			delete_nodeint_at_index(listint_t **head, unsigned int index)
 			ptr = ptr->next;
 			index--;
 		}
-		if (!index && ptr->next)
+		if (ptr && !index && ptr->next)
 		{
 			tmp = ptr->next;
 			ptr->next = tmp->next;
