e2554e0496143da05429719a3408fcd2c607364f
Nicolas Boute <4249@holbertonschool.com>
2022-05-06 09:49:03 +0200

    fixed bug with assignment 7 (bad iteration)

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,31 +2,6 @@
 
 
 /**
- * add_dnodeint - Adds node to head of doubly linked list
- * @head: Adress of head pointer to list
- * @n: Value of new node
- * Return: Pointer to new node
- */
-static dlistint_t	*add_dnodeint(dlistint_t **head, const int n)
-{
-	dlistint_t	*new;
-
-	if (!head)
-		return (NULL);
-	new = (dlistint_t *)malloc(sizeof(*new));
-	if (!new)
-		return (NULL);
-	new->prev = NULL;
-	new->n = n;
-	if (*head)
-		(*head)->prev = new;
-	new->next = *head;
-	*head = new;
-	return (new);
-}
-
-
-/**
  * insert_dnodeint_at_index - Inserts node in doubly linked list
  * @h: Adress of pointer to head of list
  * @idx: Position to insert new node at
@@ -43,7 +18,7 @@ dlistint_t	*insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!idx)
 		return (add_dnodeint(h, n));
 	ptr = *h;
-	for (i = 0; i - 1 < idx; i++)
+	for (i = 0; i + 1 < idx; i++)
 	{
 		if (!ptr)
 			return (NULL);
@@ -52,6 +27,7 @@ dlistint_t	*insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = (dlistint_t *)malloc(sizeof(*new));
 	if (!new)
 		return (NULL);
+	new->n = n;
 	new->prev = ptr;
 	new->next = ptr->next;
 	if (new->next)
