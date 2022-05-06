7081dd9a28576a204278422b004efedc1504153e
Nicolas Boute <4249@holbertonschool.com>
2022-05-06 10:51:34 +0200

    reverted to original version

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,30 @@
 #include "lists.h"
 
 /**
+ * my_add_dnodeint - Adds node to head of doubly linked list
+ * @head: Adress of head pointer to list
+ * @n: Value of new node
+ * Return: Pointer to new node
+ */
+static dlistint_t	*my_add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t	*new;
+
+	if (!head)
+		return (NULL);
+	new = (dlistint_t *)malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+	new->prev = NULL;
+	new->n = n;
+	if (*head)
+		(*head)->prev = new;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+/**
  * insert_dnodeint_at_index - Inserts node in doubly linked list
  * @h: Adress of pointer to head of list
  * @idx: Position to insert new node at
@@ -14,10 +38,8 @@ dlistint_t	*insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (!h)
 		return (NULL);
-	if (!*h)
-		return (add_dnodeint_end(h, n));
 	if (!idx)
-		return (add_dnodeint(h, n));
+		return (my_add_dnodeint(h, n));
 	ptr = *h;
 	for (i = 0; i + 1 < idx; i++)
 	{
