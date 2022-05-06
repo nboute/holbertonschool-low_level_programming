c7f153c40325c2bf7a32bcb2792542288eb3ff6c
Nicolas Boute <4249@holbertonschool.com>
2022-05-06 09:44:26 +0200

    added external function in file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,31 @@
 #include "lists.h"
 
+
+/**
+ * add_dnodeint - Adds node to head of doubly linked list
+ * @head: Adress of head pointer to list
+ * @n: Value of new node
+ * Return: Pointer to new node
+ */
+static dlistint_t	*add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t	*new;
+
+	if (!head)
+		return (NULL);
+	new = (dlistint_t *)malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+	new->prev = NULL;
+	new->n = n;
+	if (*head)
+		(*head)->prev = new;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+
 /**
  * insert_dnodeint_at_index - Inserts node in doubly linked list
  * @h: Adress of pointer to head of list
